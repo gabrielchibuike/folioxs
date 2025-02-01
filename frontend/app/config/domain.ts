bddbfa2abcb13eea0005cf501d02ba0706a2e2d7
GabbySoft <gabrielnwagu2002@gmail.com>
2025-02-02 00:32:51 +0100

    Second commit

--- frontend/app/config/domain.ts
+++ frontend/app/config/domain.ts
@@ -1,2 +1,2 @@
-// export const domain = "http://192.168.43.30:5000";
-export const domain = "http://localhost:5000";
+export const domain = "https://folioxs.onrender.com";
+// export const domain = "http://localhost:5000";
