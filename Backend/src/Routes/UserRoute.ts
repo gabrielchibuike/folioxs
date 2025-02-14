a2e42562e98ef7d9b08545c40b726ce7cc5e95a0
GabbySoft <gabrielnwagu2002@gmail.com>
2025-02-14 18:28:37 +0100

    Updated realsease

--- Backend/src/Routes/UserRoute.ts
+++ Backend/src/Routes/UserRoute.ts
@@ -12,26 +12,8 @@ import {
 } from "../controllers/UserController";
 import { upload } from "../utils/uploadFunc";
 import { verifyToken } from "../utils/verifyJwt";
-import UserDetails from "../model/UserModel";
 
 router.post("/access/create_user", create_user_controller);
-
-// router.get(
-//   "/access/uniqueDetail",
-//   verifyToken,
-//   async (req: Request, res: Response) => {
-//     // @ts-ignore
-//     const id = req.user;
-//     try {
-//       const result = await UserDetails.findOne({ _id: id });
-//       console.log(result);
-
-//       return res.status(200).send({ id: result?._id, email: result?.email });
-//     } catch (err) {
-//       res.status(200).send(err);
-//     }
-//   }
-// );
 
 router.post("/access/login", login_user_controller);
 
--- frontend/app/config/domain.ts
+++ frontend/app/config/domain.ts
@@ -1,2 +1,2 @@
-// export const domain = "https://folioxs.onrender.com";
-export const domain = "http://localhost:5000";
+export const domain = "https://folioxs.onrender.com";
+// export const domain = "http://localhost:5000";
